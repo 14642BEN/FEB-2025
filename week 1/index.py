8266b8599ab3fb1e1888f9ba00fa46de2224aab9
14642BEN <kilonzoben007@gmail.com>
2025-03-11 14:50:25 +0300

    first commit

--- week 1/index.py
+++ week 1/index.py
@@ -1,3 +1,23 @@
 print (" hello word")
 print("welcome to python programming")
 print("its amazing meeeeen!")
+
+number = 10
+print(number)
+
+
+
+
+age = 27 #integer
+marks = 90.5 # float
+name = "ben" #string
+
+
+
+
+
+
+#list data tpe
+languages = ["python", "java", "C++", "CSS", 3, 4, 5.7, True]
+print (languages)
+print (languages[-8])
